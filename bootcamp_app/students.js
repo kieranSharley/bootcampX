22ccafcf07a63758f1f61498c2ec2f2facef86c5
kieranSharley <ksharley9@gmail.com>
2020-10-15 21:29:25 +0000

    sanitzed or parameterized from attacks

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,19 +7,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-
-
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows)})
-// .catch(err => console.error('query error', err.stack));
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res.rows)})
+.catch(err => console.error('query error', err.stack));
 
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cohort 
@@ -33,16 +28,26 @@ LIMIT 2;
   })
 });
 
-//
+// //
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`,
+[`%${process.argv[2]}%`, process.argv[3] || 5])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
+
+
+// //PARAMETERIZED
+
+// const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+// // Store all potentially malicious values in an array. 
+// const values = [`%${cohortName}%`, limit];
+
